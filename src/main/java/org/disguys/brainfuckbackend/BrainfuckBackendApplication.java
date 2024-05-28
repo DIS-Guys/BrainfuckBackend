3b4cd325ab18cc3aae2c2f20350d64626055e086
Danil Kobzev <denthenttg@gmail.com>
2024-05-28 15:45:52 +0300

    Initial commit

---
+++ src/main/java/org/disguys/brainfuckbackend/BrainfuckBackendApplication.java
@@ -0,0 +1,13 @@
+package org.disguys.brainfuckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainfuckBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainfuckBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/disguys/brainfuckbackend/BrainfuckBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.disguys.brainfuckbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainfuckBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
